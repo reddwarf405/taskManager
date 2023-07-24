0d4e97207cadca35bb0d0c4559d6768cee7ffa6d
reddwarf405 <k.browncampaign@gmail.com>
2023-07-24 10:59:56 -0400

    First commit to create repo.

---
+++ taskManager.py
@@ -0,0 +1,4 @@
+import pickle
+
+def initializeGame():
+    print("Welcome to my task management game! In this game you will keep a pet alive while completing tasks throughout your day. Type 'start' to get started. Type 'help' at any time to see more commands.")
