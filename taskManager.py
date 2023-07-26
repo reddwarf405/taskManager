fd3af6966fa00391a8d60d80d911612c9100709b
reddwarf405 <k.browncampaign@gmail.com>
2023-07-26 18:48:28 -0400

    Added all ASCII art for three choices, dog, bird, and cat. Began work on task system.

--- taskManager.py
+++ taskManager.py
@@ -1,9 +1,10 @@
 import pickle
 
+
 def initializeGame():
+    global userInfo
     userInfo = {'petName' : '', 'petType' : '', 'petHunger' : 100, 'petEnjoy' : 100, 'petCleanliness' : 100}
     print("Welcome to my task management game! In this game you will keep a pet alive while completing tasks throughout your day. Type 'start' to get started. Type 'help' at any time to see more commands.")
-    print("What type of animal would you like? You may choose: dog, cat, bird.")
 
 def saveGame(playerInfo):
     with open('playerSave.pickle', 'wb') as handle:
@@ -19,6 +20,9 @@ def tryLoad():
    except:
       initializeGame()
 
+def addNewTask(): # This will create an object for the task that displays the name, the frequency of the task, and whether or not it has been completed for the day.
+   taskName = input("To add a new task, please enter the name of the task: ")
+
 # def getHelp():
    
 
@@ -32,7 +36,28 @@ def tryLoad():
 
 
 initializeGame()
+animalChosen = False
+while animalChosen == False:
+    animalChoice = input("What type of animal would you like? You may choose: dog, cat, bird.\n")
+    if animalChoice.lower() == "dog":
+       print("Great! You have selected the dog. Here it is:\n")
+       print("     __\n(___()'`;\n/,    /`\n\\\\\"--\\\\" + "\n")
+       animalChosen = True
+       userInfo["petType"] = "dog"
+    if animalChoice.lower() == "cat":
+       print("Great! You have selected the cat. Here it is:\n")
+       print(" |\__/,|   (`\\\n |_ _  |.--.) )\n ( T   )     /\n(((^_(((/(((_/")
+       animalChosen = True
+       userInfo["petType"] = "cat"
+    if animalChoice.lower() == "bird":
+       print("Great! You have selected the bird. Here it is:\n")
+       print("  , ___\n`\\/{o,o}\n / /)  )\n/,--\"-\"-")
+       animalChosen = True
+       userInfo["petType"] = "bird"
+
+
+
 
 # TODO: Add help command
-# TODO: create ASCII art for animals
+# TODO: create task objects and organization system as well as stat updates
 # TODO: make sure saves actually work
